3cec4632b37bac048f96d9f0c5a882f7ac9b5805
Shobhit-Kumar-Yadav <143309281+Shobhit-Kumar-Yadav@users.noreply.github.com>
2024-03-07 16:23:47 +0530

    Initial commit

---
+++ youtube-clone/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Navbar.css"
+
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+        <div className="nav-left flex-div">
+            <img src='' alt=''/>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
